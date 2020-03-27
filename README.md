172b9c1bb9fc32d0dcac9dca2d09de8dbfaef485
Titrom025 <titrom25@mail.ru>
2020-03-28 00:51:16 +0700

    Rebuild screen controller. Added buttons controller. Need to fix problem with idle pulse.

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 * Tic-Tac-Toe.circ - Logisim project file
 * Cdm-8-2017-mark4.circ - Cdm8 library
 * program.asm - source code from Cdm8 ROM
-* test.img - compiled and saved as img code from program.asm
+* code.img - compiled and saved as img code from program.asm
--- program.asm
+++ program.asm
@@ -1,41 +1,27 @@
 asect  0x00
 
-ldi r0, 0b11110000 
-ldi r1, 2
+#do
+ldi r0, 0b11110010
+	do
+		ld r0, r1
+		tst r1
+	until pl
+
+dec r0
+shl r1
+shl r1
+shl r1
+shl r1
+ld r2, 2
+add r2, r1
 st r0, r1
-inc r0
-st r0, r1
-inc r0
-st r0, r1
-inc r0
-st r0, r1
-inc r0
-st r0, r1
-inc r0
-st r0, r1
-inc r0
-st r0, r1
-inc r0
-st r0, r1
-inc r0
-st r0, r1
-ldi r0, 0b11110000
-ldi r1, 3
-st r0, r1
-inc r0
-st r0, r1
-inc r0
-st r0, r1
-inc r0
-st r0, r1
-inc r0
-st r0, r1
-inc r0
-st r0, r1
-inc r0
-st r0, r1
-inc r0
-st r0, r1
+#inc r0
+#push r3
+#ldi r3, 0
+#tst r3
+#pop r3
+##until mi
+
 halt
 end
 
