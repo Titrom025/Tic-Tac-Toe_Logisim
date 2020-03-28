805d53b54c2947be5cdff266800793b20d8a88bc
Titrom025 <titrom25@mail.ru>
2020-03-29 00:05:50 +0700

    Added random putting 0. Need to fix bug with erasing reg0

--- program.asm
+++ program.asm
@@ -1,26 +1,43 @@
 asect  0x00
 
-do
-ldi r0, 0b11110010
+scankeyboard:
+	ldi r0, 0b11111010
 	do
 		ld r0, r1
 		tst r1
-	until pl
+	until mi
+	
+	ld r1, r3
+	tst r3
+	bnz scankeyboard
+	
+	ldi r2, 3
+	st r1, r2
 
-dec r0
-shla r1
-shla r1
-shla r1
-shla r1
-ldi r2, 2
-add r2, r1
-st r0, r1
-inc r0
-push r3
-ldi r3, 0
-tst r3
-pop r3
-until mi
+chooserandscreen:
+	ldi r0, 0b11111011
+	ld r0, r2
+		
+	ldi r3, 9
+	cmp r3, r2
+	bmi chooserandscreen
+		
+	tst r2
+	ble chooserandscreen
+		
+	ldi r3, 0b11110000
+	or r3, r2
+		
+	ld r2, r3
+	
+	tst r3
+	
+	bnz	chooserandscreen
+	
+	ldi r3, 2
+	st r2, r3
+
+br scankeyboard
 
 halt
 end
