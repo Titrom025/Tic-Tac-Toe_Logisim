6ef801b58f5988cb3a779af1698d0237f0b94de4
Titrom025 <titrom25@mail.ru>
2020-03-28 13:19:58 +0700

    Fixed problem with pulse

--- program.asm
+++ program.asm
@@ -1,6 +1,6 @@
 asect  0x00
 
-#do
+do
 ldi r0, 0b11110010
 	do
 		ld r0, r1
@@ -8,19 +8,19 @@ ldi r0, 0b11110010
 	until pl
 
 dec r0
-shl r1
-shl r1
-shl r1
-shl r1
-ld r2, 2
+shla r1
+shla r1
+shla r1
+shla r1
+ldi r2, 2
 add r2, r1
 st r0, r1
-#inc r0
-#push r3
-#ldi r3, 0
-#tst r3
-#pop r3
-##until mi
+inc r0
+push r3
+ldi r3, 0
+tst r3
+pop r3
+until mi
 
 halt
 end
