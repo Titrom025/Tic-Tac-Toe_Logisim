07f86ef92a256a75f173c6874b2655a72a1ad45d
Roman Titkov <titrom@MacBook-Pro-Roman.local>
2020-05-26 16:06:31 +0700

    Update ROM0_program.asm

--- ROM0_program.asm
+++ ROM0_program.asm
@@ -11,58 +11,58 @@ until le
 
 scankeyboard:
 	ldi r0, 0b11111010
-	do
+	do						#waiting for button pressed
 		ld r0, r1
 		tst r1
 	until mi
 	
-	ld r1, r3
+	ld r1, r3				#test for empty screen
 	tst r3
 	bnz scankeyboard
 	
-	ldi r2, 3
+	ldi r2, 3				#set X to screen
 	st r1, r2
 	
-	ldi r1, 0b11111101
+	ldi r1, 0b11111101		#test for win combination
 	ld r1, r2
 
 	if
 	tst r2
 	is gt
-		ldi r0, 0b11111100
+		ldi r0, 0b11111100	#set winner
 		st r0, r2
 		halt
 	fi
 			
 
-chooserandscreen:
-	ldi r0, 0b11111011
+choose0move:
+	ldi r0, 0b11111011		#get a screen number for putting 0
 	ld r0, r2
 		
-	ldi r3, 0xF9
+	ldi r3, 0xF9			#check for number <= 8
 	cmp r3, r2
-	bmi chooserandscreen
+	bmi choose0move
 		
 		
-	ldi r3, 0xF0
+	ldi r3, 0xF0			#check for number >= 0
 	cmp r3, r2
-	beq chooserandscreen
+	beq choose0move
 		
 		
-	ld r2, r3
+	ld r2, r3				#check for empty screen for 0
 	tst r3
-	bnz	chooserandscreen
+	bnz	choose0move
 	
-	ldi r3, 2
+	ldi r3, 2				#set 0 to screen	
 	st r2, r3
 	
-	ldi r1, 0b11111101
+	ldi r1, 0b11111101		#test for win combination
 	ld r1, r2
 
 	if
 	tst r2
 	is gt
-		ldi r0, 0b11111100
+		ldi r0, 0b11111100	#set winner
 		st r0, r2
 		halt
 	fi
